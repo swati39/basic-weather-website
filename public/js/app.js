8f73e2c1a6f87746863de38800d1e5f197e33dae
Swati Inje <swatiinje201@gmail.com>
2022-02-03 18:10:16 +0530

    removed-unnecessary-console-log

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-console.log("Client side javascript is loaded");
-
-// fetch("https://puzzle.mead.io/puzzle").then((response) => {
-//   response.json().then((data) => {
-//     console.log(data);
-//   });
-// });
-
 //form index.hbs
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
@@ -23,10 +15,7 @@ weatherForm.addEventListener("submit", (e) => {
       response.json().then((data) => {
         if (data.error) {
           return (messageTwo.textContent = data.error);
-          console.log(data.error);
         }
-        console.log(data);
-        console.log(data.forecast);
         messageTwo.textContent = data.forecast;
       });
     }
